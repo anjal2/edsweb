c5202a2b901dc122d92c2acde15ccf161643e483
Anjali Jaswal <anjali@grazitti.local>
2024-03-22 15:51:23 +0530

    file changes

--- blocks/section-video/section-video.js
+++ blocks/section-video/section-video.js
@@ -2,8 +2,9 @@ export default function decorate(block){
 let anchorTag = document.querySelector('.section-video .button-container a');
 var video = document.createElement('video');
 video.src = anchorTag.href;
-video.autoplay="autoplay";
 video.muted="muted";
+video.autoplay="autoplay";
+video.playsInline="playsInline";
 video.setAttribute('loop', '1');
 anchorTag.parentNode.replaceChild(video, anchorTag);
 
