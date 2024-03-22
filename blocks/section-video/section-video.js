dd7f871d2f78af611e3329bf72868335c93361ef
Anjali Jaswal <anjali@grazitti.local>
2024-03-22 14:55:34 +0530

    file changes

--- blocks/section-video/section-video.js
+++ blocks/section-video/section-video.js
@@ -3,6 +3,7 @@ let anchorTag = document.querySelector('.section-video .button-container a');
 var video = document.createElement('video');
 video.src = anchorTag.href;
 video.autoplay="autoplay";
+video.loop="loop";
 anchorTag.parentNode.replaceChild(video, anchorTag);
 
 }
