182a0008309e819490ad34c10aafd604063fceb5
Anjali Jaswal <anjali@grazitti.local>
2024-03-28 13:10:39 +0530

    carousel added

---
+++ blocks/carousel/carousel.js
@@ -0,0 +1,28 @@
+<script  src="https://code.jquery.com/jquery-3.7.1.min.js"
+  integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo="
+  crossorigin="anonymous"></script>
+  
+<script   src="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/slick.min.js"></script>
+
+export default function decorate(block){
+
+    
+$('.carousel').slick({
+    slidesToShow: 4,
+    slidesToScroll: 4
+  });
+  
+  var filtered = false;
+  
+  $('.js-filter').on('click', function(){
+    if (filtered === false) {
+      $('.filtering').slick('slickFilter',':even');
+      $(this).text('Unfilter Slides');
+      filtered = true;
+    } else {
+      $('.filtering').slick('slickUnfilter');
+      $(this).text('Filter Slides');
+      filtered = false;
+    }
+  });
+}
