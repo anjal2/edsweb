01749117a491339d3bea0cd6221f95ecdf8dc59d
Anjali Jaswal <anjali@grazitti.local>
2024-03-28 13:19:12 +0530

    carousel added

--- blocks/carousel/carousel.js
+++ blocks/carousel/carousel.js
@@ -1,11 +1,11 @@
-
+<script  src="https://code.jquery.com/jquery-3.7.1.min.js"
+  integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo="
+  crossorigin="anonymous"></script>
   
 
 
 export default function decorate(block){
-<script  src="https://code.jquery.com/jquery-3.7.1.min.js"
-  integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo="
-  crossorigin="anonymous"></script>
+
     
 
 $('.carousel').slick({
--- blocks/carousel/slider.js
+++ blocks/carousel/slider.js
@@ -1,3 +1,2 @@
-export default function decorate(block){
 <script   src="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/slick.min.js"></script>
-}
+export default function decorate(block){}
